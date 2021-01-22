c13b1f18a8d0cfcb0ac8dff08157316219d7e736
bahar-hosseini <bahar.hosseini@gmail.com>
2021-01-22 15:46:50 -0500

    adding componentDidMount

--- src/index.js
+++ src/index.js
@@ -4,18 +4,15 @@ import SeasonDisplay from "./SeasonDisplay"
 
 
 class App extends React.Component{
-constructor(props){
-    super(props);
-    this.state={lat: null,errorMessage:""};
 
-window.navigator.geolocation.getCurrentPosition(
-    position=>{
-        this.setState({lat: position.coords.latitude});
-    },
-    (err)=>{
-        this.setState({errorMessage:err.message})
-    }
-);
+
+    state={lat: null,errorMessage:""};
+
+componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+        position=>this.setState({lat: position.coords.latitude}),
+        (err)=>this.setState({errorMessage:err.message})
+    );
 }
     render(){
 
