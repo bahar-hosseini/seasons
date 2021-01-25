ad89925378a170e57cf2726902598bfa45555380
Bahar Hosseini <bahar.hosseini@gmail.com>
2021-01-25 13:24:09 -0500

    second commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import SeasonDisplay from "./SeasonDisplay"
+import "./SeasonDisplay.css"
+import Spinner from "./Spinner";
 
 
 class App extends React.Component{
@@ -14,15 +16,18 @@ componentDidMount(){
         (err)=>this.setState({errorMessage:err.message})
     );
 }
-    render(){
+renderContent(){
 
-        if(this.state.errorMessage && !this.state.lat){
-            return <div>Error:{this.state.errorMessage}</div>
-        }
-        if(!this.state.errorMessage && this.state.lat){
-            return<div>Latitude:{this.state.lat}</div>
-        }
-        return<div>Loading!</div>
+    if(this.state.errorMessage && !this.state.lat){
+        return <div>Error:{this.state.errorMessage}</div>
+    }
+    if(!this.state.errorMessage && this.state.lat){
+      return <SeasonDisplay lat={this.state.lat}/ >
+    }
+    return <Spinner message="Please accept location request"/ >
+}
+    render(){
+return <div className="">{this.renderContent()}</div>
     }
 }
 
